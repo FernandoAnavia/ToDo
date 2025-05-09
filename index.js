54a5063ef072ff036798c3e9b3217f1af0e36ff4
FernandoAnavia <61196886+FernandoAnavia@users.noreply.github.com>
2025-05-09 17:36:09 -0600

    Aplicación final

--- index.js
+++ index.js
@@ -68,8 +68,21 @@ app.post('/agrega_todo', jsonParser, function (req, res) {
 
 app.get('/', function (req, res) {
     //Enviamos de regreso la respuesta
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ 'status': 'Todo es correcto :)' }));
+    //res.setHeader('Content-Type', 'application/json');
+    //res.end(JSON.stringify({ 'status': 'Todo es correcto ok:)' }));
+
+    const sql = 'SELECT * FROM todos';
+
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error("Error al consultar los basa de datos:", err.message);
+            res.status(500).json({ error: 'Error al consultar las tareas' });
+            return;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(rows);
+    })
 })
 
 
@@ -82,6 +95,12 @@ app.post('/login', jsonParser, function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ 'status': 'tarea agregada' }));
 })
+
+// Endpoint para obtener todos los TODOs
+app.get('/lista_todos', function (req, res) {
+
+})
+
 
 //Corremos el servidor en el puerto 3000
 const port = 3000;
